8034dbfa56866ba86d50f358ae2e3c7f2fd9fb76
Fatih Kadir Akın <fatihkadirakin@gmail.com>
2018-12-02 04:13:24 +0300

    update readme

--- example/App.vue
+++ example/App.vue
@@ -1,11 +1,10 @@
 <template>
   <div>
-    <input type="text" v-model='query' v-smart-routes='routes'>
+    <input placeholder="search or run something..." type="text" v-model='query' v-smart-routes='routes'>
     <ul>
       <li @click='route.handler' v-for='route in routes' :key='route.name + Math.random()' v-html='route.title' />
     </ul>
     <hr>
-    <h4>Router View:</h4>
     <router-view></router-view>
   </div>
 </template>
@@ -20,3 +19,49 @@ export default {
   }
 }
 </script>
+
+<style>
+body {
+  padding: 20px;
+}
+* {
+  font-family: Avenir, sans-serif;
+}
+input {
+  font-size: 1.5rem;
+  padding: 0.5rem;
+  width: 100%;
+  border-radius: 0.5rem;
+  border: 1px solid rgb(230, 230, 230);
+  outline: none;
+  box-sizing: border-box;
+}
+
+ul {
+  list-style: none;
+  padding: 0;
+  background-color: rgb(250, 250, 250);
+  border-radius: 5px;
+  padding: 10px;
+}
+
+li {
+  display: block;
+  margin: 4px;
+  padding: 4px 10px;
+  cursor: pointer;
+  border-radius: 4px;
+}
+
+li:hover {
+  background-color: pink;
+}
+
+mark {
+  background-color: red;
+  color: #fff;
+  display: inline-block;
+  padding: 2px 5px;
+  border-radius: 3px;
+}
+</style>
